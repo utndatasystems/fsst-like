476e344e47a9119c8a4c14bb111ec63216d06253
Mihail Stoian <stoianmmihail@yahoo.com>
2025-05-22 08:46:19 +0200

    fixed patterns with spaces

--- src/FsstWrapper.hpp
+++ src/FsstWrapper.hpp
@@ -46,7 +46,7 @@ public:
    // to be able to compare with encoded tuple data. Hence, this method.
    std::pair<bool, uint32_t> Encode(std::string_view text, std::span<char> output) const;
 
-   std::string SymbolToStr(unsigned code_index) const;
+   std::string SymbolToStr(unsigned code_index, bool debug = false) const;
    void PrintSymbolTable(std::ostream& os) const;
 
    uint32_t GetSymbolTableSize() const { return symbol_table_size; }
